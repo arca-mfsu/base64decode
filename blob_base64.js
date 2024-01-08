f0e75c3674680ecc6e3592f198fae89eda803e42
arca-mfsu <155862527+arca-mfsu@users.noreply.github.com>
2024-01-08 12:59:38 +0900

    Create blob_base64.js
    copy from
    https://gist.github.com/Hagith/6521777

---
+++ blob_base64.js
@@ -0,0 +1,23 @@
+// source: http://jsperf.com/blob-base64-conversion
+function base64ToBlob(base64) {
+        var binary = atob(base64);
+        var len = binary.length;
+        var buffer = new ArrayBuffer(len);
+        var view = new Uint8Array(buffer);
+        for ( var i = 0; i < len; i++) {
+                view[i] = binary.charCodeAt(i);
+        }
+        var blob = new Blob([ view ]);
+        return blob;
+};
+
+
+function blobToBase64(blob, cb) {
+    var reader = new FileReader();
+    reader.onload = function() {
+      var dataUrl = reader.result;
+      var base64 = dataUrl.split(',')[1];
+      cb(base64);
+    };
+    reader.readAsDataURL(blob);
+};
